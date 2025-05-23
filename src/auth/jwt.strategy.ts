1f28e25ba739ce5ae86ae7cb6a7964c02348ddc9
ridho <adiridho94@gmail.com>
2025-05-24 02:16:51 +0700

    fix

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,22 +1,22 @@
-// src/auth/jwt.strategy.ts
-import { Injectable } from '@nestjs/common'
-import { PassportStrategy } from '@nestjs/passport'
-import { Strategy, ExtractJwt } from 'passport-jwt'
-
-@Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy) {
-    constructor() {
-        super({
-            jwtFromRequest: ExtractJwt.fromExtractors([
-                req => req?.cookies?.jwt,                           // dari cookie
-                ExtractJwt.fromAuthHeaderAsBearerToken(),          // fallback header
-            ]),
-            ignoreExpiration: false,
-            secretOrKey: process.env.JWT_SECRET,                  // dari .env
-        })
-    }
-
-    async validate(payload: any) {
-        return { userId: payload.sub, username: payload.username }
-    }
-}
+// // src/auth/jwt.strategy.ts
+// import { Injectable } from '@nestjs/common'
+// import { PassportStrategy } from '@nestjs/passport'
+// // import { Strategy, ExtractJwt } from 'passport-jwt'
+//
+// @Injectable()
+// export class JwtStrategy extends PassportStrategy(Strategy) {
+//     constructor() {
+//         super({
+//             jwtFromRequest: ExtractJwt.fromExtractors([
+//                 req => req?.cookies?.jwt,                           // dari cookie
+//                 ExtractJwt.fromAuthHeaderAsBearerToken(),          // fallback header
+//             ]),
+//             ignoreExpiration: false,
+//             secretOrKey: process.env.JWT_SECRET,                  // dari .env
+//         })
+//     }
+//
+//     async validate(payload: any) {
+//         return { userId: payload.sub, username: payload.username }
+//     }
+// }
